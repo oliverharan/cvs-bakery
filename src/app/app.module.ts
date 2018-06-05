a01cab1ee311d7655ee42ef507c1a597261422e5
Oliver Haran <harano@live.com>
2018-06-05 11:10:10 -0400

    data.json?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CvsCookerModule} from './cvs-cooker/cvs-cooker.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CvsCookerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
